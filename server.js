5350127e1ad72dcee6ee48e1dcf2d02e66c59c31
Abdulaziz Alqahtani <h-xa2012@hotmail.com>
2021-11-16 00:48:47 +0300

    Adding logger & controllers

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
+var morgan = require("morgan");
+// Routes files
+const bootcamps = require("./routes/bootcamps");
 // Load the config file
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+
+// Dev logging
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+// Attatch routes
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
